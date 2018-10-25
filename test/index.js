ba17ec8eefac13018df5ef2f8462358a5b14daba
jsonchou <zhoulongqiang@zhongan.com>
2018-10-25 15:34:48 +0800

    add diff fn

--- test/index.js
+++ test/index.js
@@ -7,10 +7,13 @@ console.log(Object.keys(zaxDate))
 console.log(zaxDate.compare('2018/9/10'))
 
 // offset
-console.log(zaxDate.offset('2018/9/10','month',2))
+console.log(zaxDate.offset('2018/9/10', 'month', 2))
 
 // ago
 console.log(zaxDate.ago('2018/9/11'))
 
 // format
-console.log(zaxDate.format('2018/10/25','yyyy-mm-dd HH:MM'))
+console.log(zaxDate.format('2018/10/25', 'yyyy-mm-dd HH:MM'))
+
+// diff
+console.log(zaxDate.diff('2018/10/27', '2018/10/26'))
